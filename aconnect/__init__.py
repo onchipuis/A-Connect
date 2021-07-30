c8af87a5939a9494aa6bc7e28ea6153138145b7c
rvergel <ricamatheo98@gmail.com>
2021-07-30 15:15:59 -0500

    fixed functions and laod_ds dtype

--- aconnect/__init__.py
+++ aconnect/__init__.py
@@ -1,12 +1,4 @@
-from layers import FC_AConnect
-from layers import Conv_AConnect
-from scripts import MonteCarlo
-from scripts import classify
-from scripts import load_ds
-from scripts import plotBox
-import scripts 
-import layers
-__version__ = "1.3.3"
+__version__ = "1.3.5"
 __author__ = 'Ricardo Vergel, Edward Silva, Luis Rueda and Elkim Roa'
 def __init__():
 	print("aconnect library")
