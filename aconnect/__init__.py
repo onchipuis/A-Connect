b2ef4f390e1b2198a1ab86f9b2819015b5289574
rvergel <ricamatheo98@gmail.com>
2021-07-30 14:28:56 -0500

    testing new way to import the library

--- aconnect/__init__.py
+++ aconnect/__init__.py
@@ -1,4 +1,14 @@
-__version__ = "1.2.0"
-__author__ = 'Ricardo Vergel, Edward Silva and Luis Rueda'
+from layers import FC_AConnect
+from layers import Conv_AConnect
+from scripts import MonteCarlo
+from scripts import classify
+from scripts import load_ds
+from scripts import plotBox
+import scripts 
+import layers
+__version__ = "1.3.3"
+__author__ = 'Ricardo Vergel, Edward Silva, Luis Rueda and Elkim Roa'
 def __init__():
 	print("aconnect library")
+	
+	
